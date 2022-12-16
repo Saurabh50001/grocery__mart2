739a987f5aa8c6ec94be76d99ba1a8c28a9a7af4
Saurabh Kumar <sks201998@gmail.com>
2022-12-17 02:10:41 +0530

    deleted word file

--- config.php
+++ config.php
@@ -1,6 +1,20 @@
 <?php 
 	error_reporting(E_ERROR | E_PARSE);
-	$conn=mysqli_connect('localhost',"root","","cakebake");
+	$conn = mysqli_connect('localhost',"root","","cakebake");
 	//echo("Database connected<br>");
 	session_start();
  ?>
+
+<?php
+    // error_reporting(E_ERROR | E_PARSE);
+
+    // $conn = mysqli_connect('localhost','root','');
+        
+    // if (!$conn) 
+	// {
+    //     die('Could not connect: ' . mysqli_error($conn));
+    // }
+	
+    // mysqli_select_db($conn,"cakebake");    
+    
+?>
